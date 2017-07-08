687c5d516c31161ec6ac9c4d29ee35b256396c70
Victor <jvllopess@gmail.com>
2017-07-08 13:52:45 -0400

    Adiciona arquivo "perfil.png" e coloca na tela

--- app/src/main/java/tk/trocagame/trocagame/view/fragments/PerfilFragment.java
+++ app/src/main/java/tk/trocagame/trocagame/view/fragments/PerfilFragment.java
@@ -8,13 +8,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import tk.trocagame.trocagame.R;
+import tk.trocagame.trocagame.model.Usuario;
+import tk.trocagame.trocagame.utils.LocalStorage;
 
 
 public class PerfilFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private TextView name;
+    private TextView login;
+    private TextView dercricao;
+    private TextView telefone;
+
 
 
     @Override
@@ -28,6 +36,15 @@ public class PerfilFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_perfil, container, false);
+
+
+        Usuario usuario = LocalStorage.getInstance(this).getObject(LocalStorage.ACTIVE_USER, Usuario.class);
+
+        View header = navigationView.getHeaderView(0);
+        name = (TextView) header.findViewById(R.id.tv_name);
+        login = (TextView) header.findViewById(R.id.tv_login);
+        name.setText(usuario.getNome());
+        login.setText(usuario.getLogin());
     }
 
 }
