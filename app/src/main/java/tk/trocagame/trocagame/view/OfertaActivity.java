a0d0eae4ac7cdf0cf077b403935115693eb640a7
Victor <jvllopess@gmail.com>
2017-07-22 18:24:10 -0400

    Adiciona regras de negocio da oferta

--- app/src/main/java/tk/trocagame/trocagame/view/OfertaActivity.java
+++ app/src/main/java/tk/trocagame/trocagame/view/OfertaActivity.java
@@ -2,15 +2,28 @@ package tk.trocagame.trocagame.view;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import tk.trocagame.trocagame.R;
 
 public class OfertaActivity extends Activity {
 
+    TextView text_title;
+    EditText ano_compra;
+    EditText estado_jogo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_oferta );
+
+        text_title = (TextView) findViewById(R.id.textView_title);
+        ano_compra = (EditText) findViewById(R.id.editText_ano_compra);
+        estado_jogo = (EditText) findViewById(R.id.editText_estado_jogo);
+
+
+
     }
 
 
